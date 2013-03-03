120480fe1681d33460978ec455f6cb2fbdb6b2df
Luke Maurits <luke@maurits.id.au>
2013-03-03 10:24:42 -0800

    Initial import.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 gopherfeed
 ==========
 
-Convert RSS or Atom feeds to gophermap files
+Convert RSS or Atom feeds to gophermap files.
+
+Usage: gopherfeed <feed_url> [<filename>]
+
+Will translate contents of RSS or Atom feed at <feed_url> into the
+Gophermap format and save the result to <filename> if specified,
+otherwise will print to stdout.
---
+++ gopherfeed.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2013, Luke Maurits <luke@maurits.id.au>
+# Published under the terms of the BSD 3-Clause License
+# (see LICENSE file or http://opensource.org/licenses/BSD-3-Clause)
+
+import feedparser
+import sys
+
+def gopherize_feed(feed_url):
+    maplines = []
+    f = feedparser.parse(feed_url)
+    maplines.append("%s" % f.feed.title.replace("\t","    "))
+    if f.feed.description:
+        maplines.append(f.feed.description.replace("\t","    "))
+    for entry in f.entries:
+        filetype = "h"
+        maplines.append("%s%s\tURL:%s" % (filetype, entry.title.replace("\t","   "), entry.link))
+    gophermap = "\n".join(maplines)
+    return gophermap
