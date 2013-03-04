86026f1a55214afb78d8c33a79ec7edea7b32554
Luke Maurits <luke@maurits.id.au>
2013-03-03 20:42:23 -0800

    Check more carefully whether or not values are set before trying to use them.

--- gopherfeed.py
+++ gopherfeed.py
@@ -3,13 +3,16 @@
 # (see LICENSE file or http://opensource.org/licenses/BSD-3-Clause)
 
 import feedparser
+
 import sys
 
 def gopherize_feed(feed_url):
     maplines = []
     f = feedparser.parse(feed_url)
+    if "title" not in f.feed:
+        return ""
     maplines.append("%s" % f.feed.title.replace("\t","    "))
-    if f.feed.description:
+    if "description" in f.feed:
         maplines.append(f.feed.description.replace("\t","    "))
     for entry in f.entries:
         filetype = "h"
