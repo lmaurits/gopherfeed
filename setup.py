c9014694a0f404f551c69f4c6b3ed655a29baa93
Luke Maurits <luke@maurits.id.au>
2013-03-03 22:00:38 -0800

    Added setup.p

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='gopherfeed',
+    version='1.0',
+    description='Convert RSS or Atom feeds to gophermap files',
+    author='Luke Maurits',
+    author_email='luke@maurits.id.au',
+    url='https://github.com/lmaurits/gopherfeed',
+    license="BSD (3 clause)",
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+    ],
+    scripts=['bin/gopherfeed',],
+    py_modules=['gopherfeed',],
+    requires=['feedparser'],
+    install_requires=['feedparser']
+)
