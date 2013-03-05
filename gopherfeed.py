85dcc90c751f39a419725a7d5e4819bd1a64c00d
Luke Maurits <luke@maurits.id.au>
2013-03-04 22:15:29 -0800

    Made some changes recommended by pylint.  Mostly docstrings and other small niceties.

--- gopherfeed.py
+++ gopherfeed.py
@@ -6,9 +6,9 @@ import feedparser
 
 import codecs
 import os
-import sys
 
 def _gopherize_feed_object(feed):
+    """Return a gophermap string for a feed object produced by feedparser."""
     maplines = []
     if "title" not in feed.feed:
         return ""
@@ -17,14 +17,17 @@ def _gopherize_feed_object(feed):
         maplines.append(feed.feed.description.replace("\t","    "))
     for entry in feed.entries:
         filetype = "h"
-        maplines.append("%s%s\tURL:%s" % (filetype, entry.title.replace("\t","   "), entry.link))
+        descr = entry.title.replace("\t","   ")
+        maplines.append("%s%s\tURL:%s" % (filetype, descr, entry.link))
     gophermap = "\n".join(maplines)
     return gophermap
 
 def gopherize_feed(feed_url):
+    """Return a gophermap string for the feed at feed_url."""
     return _gopherize_feed_object(feedparser.parse(feed_url))
 
 def _slugify(feed):
+    """Make a simple string from feed title, to use as a directory name."""
     slug = feed.title
     slug = slug.encode("ASCII", "ignore")
     for kill in """.,:;-"'\`\/""":
@@ -34,28 +37,39 @@ def _slugify(feed):
     return slug
 
 def _read_feed_urls(filename):
+    """Return a list of URLs read from a file, one per line."""
     feeds = []
     fp = codecs.open(filename, "r", "UTF-8")
     for line in fp:
+        # Ignore blank and commented lines
         if line and not line.startswith("#"):
             feeds.append(line)
     fp.close()
     return feeds
 
 def gopherize_feed_file(feedfile, directory, hostname, port=70):
+    """
+    Read a file of URLs and generate a directory structure of gophermaps.
+
+    This method will read URLs from a file and then create a directory
+    structure, in which each feed gets its own directory.  Goperhmap files
+    will be created for each feed (in that feed's directory), along with one
+    master gophermap file at the root of the directory structure which acts
+    as an index to the others (using each feed's title as the descriptor).
+    """
     feeds = _read_feed_urls(feedfile)
     fp = codecs.open(directory+"/"+"gophermap", "w", "UTF-8")
     for feed_url in feeds:
-	feed = feedparser.parse(feed_url)
-	if "title" not in feed.feed:
-    	    continue
-	feed_slug = _slugify(feed.feed)
-	dir = directory + "/" + feed_slug
-	gophermap = dir + "/" + "gophermap"
-	if not os.path.exists(dir):
-    	    os.mkdir(dir)
-	fp.write("1%s\t%s\t%s\t%d\n" % (feed.feed.title, dir, hostname, port))
+        feed = feedparser.parse(feed_url)
+        if "title" not in feed.feed:
+            continue
+        feed_slug = _slugify(feed.feed)
+        directory = directory + "/" + feed_slug
+        gophermap = dir + "/" + "gophermap"
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        fp.write("1%s\t%s\t%s\t%d\n" % (feed.feed.title, dir, hostname, port))
         fp2 = codecs.open(gophermap, "a", "UTF-8")
-	fp2.write(_gopherize_feed_object(feed))
+        fp2.write(_gopherize_feed_object(feed))
         fp2.close()
     fp.close()
