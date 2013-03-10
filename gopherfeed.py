cec2d15f4187924462c4713169117b6bf2d34556
Luke Maurits <luke@maurits.id.au>
2013-03-10 14:33:07 -0700

    Include plug line in main index Gophermap too.

--- gopherfeed.py
+++ gopherfeed.py
@@ -72,7 +72,8 @@ def _slugify(feed):
         slug = slug.lower()
     return slug
 
-def build_feed_index(feed_objects, directory, hostname=None, port=70, sort=None):
+def build_feed_index(feed_objects, directory, hostname=None, port=70, sort=None,
+        plug=True):
     """
     Build a gophermap file in the specified directory, which presents an index
     for all the feeds in feed_objects.
@@ -102,4 +103,9 @@ def build_feed_index(feed_objects, directory, hostname=None, port=70, sort=None)
     fp = codecs.open(os.path.join(directory, "gophermap"), "w", "UTF-8")
     for decoration, mapline in decorated_maplines:
         fp.write(mapline)
+    if plug:
+        maplines.append("_"*70)
+        plug_line = "Converted from RSS/Atom feeds by Gopherfeed %s" % (feed_type, __version__)
+        maplines.append(plug_line.rjust(70))
+    maplines.append("")
     fp.close()
